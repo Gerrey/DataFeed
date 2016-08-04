9672737f66f8b9f554c725b864adbb99aecf5296
赵轻盈 <zhaoxf2005@163.com>
2016-08-04 18:58:15 +0800

    Revert "Revert "first submit""
    This reverts commit 7c15f2d5c728fa20bec84ea05ae7617a34359a3b.

---
+++ DataFeed/DataFeed/DataFeed/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataFeed
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WinSCPClient client = new WinSCPClient();
+            client.SFTPUploadFiles();
+        }
+    }
+}
